962b401fc7f3c75606bd71951aa07b971154ddca
mauro-balades <mauro.balades@tutanota.com>
2022-01-01 13:49:17 +0100

    added the ability to add / sub in a cell

--- src/brainfuck.rs
+++ src/brainfuck.rs
@@ -1,11 +1,31 @@
 use std::mem;
+use std::str::Chars;
 
 pub fn brainfuck(programm: String) {
-    let cells: [i32; 3000];
-    let possition: i32 = 0;
+    let mut cells: [i32; 3000] = [0; 3000];
+    let possition: &mut usize = &mut 0;
+    let chars: Chars = programm.chars();
 
-    for (i, c) in programm.chars().enumerate() {
+    let mut index: i32 = 1;
+    while index < chars.clone().count().try_into().unwrap() {
+        let cur_char = chars.clone().nth(index.try_into().unwrap()).unwrap();
 
+        match cur_char {
+            // Increment value by 1 in current cell possition
+            '+' => cells[*possition] += 1,
+
+            // Decrement value by 1 in current cell possition
+            '-' => cells[*possition] -= 1,
+
+            // In Brainfuck, other ASCII characters that
+            // are not ["+", ",", "-", "<", ">", ".", "[", "]"]
+            // they are considered as comments, so we do nothing.
+            _ => (),
+        }
+
+        index += 1;
     }
+
+    println!("{:?}", cells);
 
 }
