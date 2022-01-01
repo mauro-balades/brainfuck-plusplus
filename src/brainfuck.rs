0c6f7df42e83bcb793a295ac59d9e3785600c849
mauro-balades <mauro.balades@tutanota.com>
2022-01-01 13:12:52 +0100

    started brainfuck tests

---
+++ src/brainfuck.rs
@@ -0,0 +1,5 @@
+use std::mem;
+
+pub fn brainfuck() {
+    let cells: [i32; 3000];
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-use std::env;
 
-fn brainfuck() {
-    
-}
+
+use std::io;
+use std::io::prelude::*;
+use std::env;
+use std::fs::*;
+use std::path::Path;
+
+mod brainfuck;
+
+pub use brainfuck::*;
 
 fn main() {
     // Get command line arguments
@@ -17,6 +23,25 @@ fn main() {
         // TODO: check for more commands
         let filename = &args[1];
 
+        // check if file eixsts
+        if Path::new(filename).exists() {
+
+            // Open file
+            let mut file = File::open(&filename).expect("Error opening File");
+
+            // Define file's contents as a new String
+            let mut contents = String::new();
+
+            // Read file's contents and store it as a String
+            // in [contents]. If it does not work, throw an
+            // error.
+            file.read_to_string(&mut contents).expect("Unable to read to string");
+
+
+            return
+        }
+
+        // TODO: print error
 
     } else {
         // Error, filename was not given
